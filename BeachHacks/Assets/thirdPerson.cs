4ebde48085b0bc68c625a97f17b0de8d1db2aeb3
BlackSwiss <dylan.ayala562@gmail.com>
2019-04-20 16:55:24 -0700

    camera(PROGRESS) controls (WORKING)

---
+++ BeachHacks/Assets/thirdPerson.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class thirdPerson : MonoBehaviour
+{
+    public Transform target;
+    public float walkDistance;
+    public float runDistance;
+    public float height;
+
+    private Transform _myTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (target == null)
+            Debug.LogWarning("No target detected");
+        _myTransform = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void LateUpdate()
+    {
+        _myTransform.position = new Vector3(target.position.x, target.position.y + height, target.position.z - walkDistance);
+    }
+}
